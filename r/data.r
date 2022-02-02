b4e999f3e2216da62ddc8bc1f02f5effd0116cb9
emuise <evanmuise@gmail.com>
2022-02-02 14:45:27 -0800

    updated to be my cv

--- r/data.r
+++ r/data.r
@@ -1,101 +1,62 @@
 
 education <- tribble(
     ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-    "University of British Columbia", "PhD Remote Sensing of Forests", "May", 2015, "March", 2019, "Vancouver, BC",
-    "Assessing the role of digital aerial photogrammetry for characterizing forest structure and enhancing forest inventories",
-    "University of British Columbia", "BSc Natural Resources Conservation (Hons.\\& Co-op)", "September", 2010, "January", 2015, "Vancouver, BC",
-    "Science and Management Major. \\begin{itemize}
-        \\item Cons 330: Conservation Policy
-        \\item FRST 443: Remote Sensing Of Ecosystem Management
-        \\item CONS 451: Integrated Field School
-    \\end{itemize}"
-)
+    "University of British Columbia", "MSc Remote Sensing and Conservation", "June", 2020, "Present", NA, "Vancouver, BC",
+    "Monitoring Forest Structural Integrity Across a Range of Regional Ecosystems",
+    "Dalhousie University", "BSc Environmental Science (Hons)", "September", 2015, "May", 2020, "Halifax, NS",
+    "Emerging Hot Spot Analysis and Forests: A Case Study on the Hemlock Woolly Adelgid’s Invasion into Nova Scotia using Fine Spatial Resolution Satellite Imagery."
+    )
 
 
 skills <- tribble(
     ~area, ~skills,
     "Project Management", "Effective Planning, Organization, Communication, Implementation",
     "Technical Writing", "Publications, Official Reports, Funding Grants",
-    "Data Analytics", " R, Management, Manipulation, Visualization, Interpretation",
+    "Data Analytics", "R, Management, Manipulation, Visualization, Interpretation",
     "Visual Communitation", "Effective Scientific Graphics and Design",
 )
 
 awards <- tribble(
     ~area, ~accomplishment, ~year, ~where, ~detail,
-    "PhD Funding", "NSERC PGSD", 2018, "Vancouver, BC", NA,
-    "PhD Funding", "Harry G. Smith Scholarship", 2018, "Vancouver, BC", NA
+    "Undergraduate Research Funding", "NSERC USRA", 2018, "Halifax, NS", "Funding for Urban Forestry Summer Internship with REMLab"
 )
 
 experience <- tribble(
     ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-    "Post-Doctoral Fellow", "Faculty of Forest Resources Management (UBC)", "June", 2019, "Present", NA, "Vancouver, BC",
-    "Assessment of wood attributes using remote sensing (AWARE) \\& Canadian Wood Fibre Center Forest Innovation Program. \\begin{itemize}
-        \\item Mentor graduate students on remote sensing practices and routines
-        \\item Developed lidar processing and analysis frameworks to enhance forest inventory knowledge
-        \\item Developed a structurally guided sampling package sgsR
-        \\item Published in high impact journals with high rate of citations
+    "MSc Student", "Faculty of Forest Resources Management (UBC)", "June", 2020, "Present", NA, "Vancouver, BC",
+    "Monitoring Forest Structural Integrity Across a Range of Regional Ecosystems. \\begin{itemize}
+        \\item Identified placement bias in the British Columbian protected area network for ecosystem and land cover classes
+        \\item Identified missing forest structures in the British Columbian protected area network across a range of ecosystems
     \\end{itemize}",
-    "Teaching assistant", "Faculty of Forest Resources Management (UBC)", "May", 2015, "March", 2019, "Vancouver, BC",
-    "Leading \\& supervising applied exercises to guide student knowledge of remote sensing concepts. \\begin{itemize}
-        \\item Guiding students to success in written and oral submissions 
-        \\item FRST 443 - Remote sensing for ecosystem management
-        \\item FRST 521 - Advanced earth observation and image processing
-        \\item Held weekly office hours, guest lectured, developed and graded assignments and research proposals
+    "Teaching Assistant", "Faculty of Forest Resources Management (UBC)", "September", 2020, "Present", NA, "Vancouver, BC",
+    "Leading \\& supervising applied exercises to guide student knowledge of remote sensing and coding concepts. \\begin{itemize}
+        \\item GEM 530 - Geospatial Data Analysis (Fall 2020)
+        \\item CONS 127 - Observing the Earth from Space (Spring 2021)
+        \\item CONS 340 - Introduction to Geographic Information Systems for Forestry and Conservation
+        \\item CONS127 - Observing the Earth from Space (Head TA; Spring 2022)
     \\end{itemize}",
-    "Planning Forester Intern - Co-op", "Chetwynd Forest Industries - West Fraser Mills LTD.", "May", 2014, "December", 2014, "Chetwynd, BC",
-    "Working in a team and individually to implement West Fraser Management objectives. \\begin{itemize}
-        \\item Site plan preparation
-        \\item Harvest planning \\& layout
-        \\item Silviculture assessments and reforestation quality inspections
-        \\item post-fire salvage planning and management
-        \\item GIS management
-        \\item Forest appraisals
+    "Teaching Assistant", "Department of Earth and Environmental Science (Dalhousie)", "January", 2020, "May", 2020, "Halifax, NS",
+    "Leading \\& supervising applied exercises to guide student knowledge of environmental issues in the earth sciences. \\begin{itemize}
+        \\item ENVS2410 - Environmental Issues in Earth Sciences
+        \\item Introduced and aided students with activities related to mapping and environmental contaminants.
     \\end{itemize}",
-    "Planning Forester Intern - Co-op", "Alex Fraser Research Forest (UBC)", "May", 2013, "September", 2013, "Williams Lake, BC",
+    "Junior Program Officer (PM-01)", "Indigenous Partnerships Office - West (NRCan)", "September", 2018, "May", 2019, "Vancouver, BC",
     "Implemented planning and management perscriptions. \\begin{itemize}
-        \\item Road \\& timber layout
-        \\item Timber cruising
-        \\item Volume determination
-        \\item Avian habitat assessments
+        \\item Report Preparation
+        \\item Data Maintenance and Cleaning
+        \\item Liasions with Indigenous Groups
+    \\end{itemize}",
+    "Undergrad Research Assistant", "REMLab (Dalhousie)", "May", 2018, "August", 2018, "Halifax, NS",
+    "Organized and conducted urban forestry field campaigns. \\begin{itemize}
+        \\item Data Collection, Entry, Cleaning, and Analysis
+        \\item Public Outreach Regarding Urban Forests
     \\end{itemize}"
 )
 
 presentations <- tribble(
     ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
-    "AWARE E-lecture Series", "Digital Photogrammetric Applications to Enhanced Forest Inventory", "October", 2019, NA, NA, "Online",
+    "Documenting Structural Land Cover Change and Disturbance across Provincial BC Parks for Biodiversity Assessment", "BC Parks Living Lab Colloquium", "December", 2020, NA, NA, "Online",
     NA,
-    "Silvilaser", "Uncovering spatial and ecological variability in gap size frequency distributions in the Canadian boreal forest", "October", 2019, NA, NA, "Foz de Iguazu, Brazil",
-    NA,
-    "CIF-IFC Workshop", "Examining potential applications of UAS and digital photogrammetry for various forest management purposes", "April", 2019, NA, NA, "Edmonton, AB",
-    NA,
-    "FRI Forest Practitioners Conference", "Assessing the status of forest regeneration using digital aerial photogrammetry and unmanned aerial systems", "October", 2018, NA, NA, "Edmonton, AB",
-    NA,
-    "AWARE Instructional Sessions", "LiDAR theory, processing and implementation workshop", "February", 2018, NA, NA, "Kamloops, BC",
-    NA,
-    "AWARE Instructional Sessions", "LiDAR theory, processing and implementation workshop", "October", 2017, NA, NA, "Huntsville, ON",
-    NA,
-    "AWARE Instructional Sessions", "LiDAR theory, processing and implementation workshop", "October", 2017, NA, NA, "Kapuskasing, ON",
-    NA,
-    "AWARE Instructional Sessions", "LiDAR theory, processing and implementation workshop", "October", 2017, NA, NA, "Quesnel, BC",
-    NA,
-    "Silvilaser", "Assessing the capacity of DAP to enhance inventory knowledge of Spruce Budworm affected forests", "November", 2017, NA, NA, "Blacksburg, USA",
-    NA,
-    "Symposium on Systems and Analysis in Forest Resources", "Updating airborne laser scanning EFI metrics using UAV acquired DAP point clouds", "August", 2017, NA, NA, "Suquamish, USA",
-    NA,
-    "UAV and Remote Sensing Workshop", "UAV and Digital Photogrammetry for forestry purposes", "July", 2017, NA, NA, "Nanjing, China",
-    NA,
-    "Assessment of Wood Attributes for Remote Sensing AGM", "Assessing the capacity of DAP to enhance inventory knowledge of Spruce Budworm affected forests", "May", 2017, NA, NA, "Edmunston, NB",
-    NA,
-    "ForestSAT", "Modelling residual stand volume using unmanned aerial vehicles and digital aerial photogrammetry", "November", 2016, NA, NA, "Santiago, Chile",
-    NA,
-    "FP Innovations UAV Workshop", "UAVs and the University of British Columbia", "October", 2016, NA, NA, "Courtney, BC",
-    NA,
-    "Canadian Remote Sensing Symposium", "Modelling residual stand volume using unmanned aerial vehicles and digital aerial photogrammetry", "June", 2016, NA, NA, "Winnepeg, MB",
-    NA,
-    "Southern Interior Silviculture Committee", "DAP point clouds acquired from Unmanned Aerial Systems (UAS) for enhancing forest inventories", "February", 2016, NA, NA, "Kamloops, BC",
-    NA,
-    "Alex Fraser Research Forest Proof of Concept Workshop", "Research in use of drones to update LiDAR forest inventories. LiDAR High Resolution Inventory for the IDF", "June", 2015, NA, NA, "Williams Lake, BC", NA
-    
 )
 
 
